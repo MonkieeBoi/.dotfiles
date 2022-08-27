01ba2bdc69842d601ac645388771bf2ffd594666
MonkieeBoi <danny.pham26425@gmail.com>
2022-08-27 16:20:20 +1000

    small visual tweak

--- .suckless/dwmblocks/blocks.def.h
+++ .suckless/dwmblocks/blocks.def.h
@@ -1,11 +1,11 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"",   "date '+%a %r %F'",		                                    	    1,		0},
+	{"",   "date '+%a %r %D'",		                                    	    1,		0},
 	{" ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	        10,		14},
 	{" ", "amixer sget Master | awk -F\"[][]\" '/%/ { print $2 }' | head -n1", 0,		10},
 	{" ", "echo -n \"$(cat /sys/class/power_supply/BAT1/capacity)%\"",	        120,		3},
-    {" ", "echo -n \"$(($(brightnessctl get)*100/$(brightnessctl max)))%\"",   0,      10},
+    {" ", "echo -n \"$(($(brightnessctl get)*100/$(brightnessctl max)))% |\"",   0,      10},
 
 };
 
