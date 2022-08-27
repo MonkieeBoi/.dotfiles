288f914446b824fde280d6251181378cc3deb158
MonkieeBoi <danny.pham26425@gmail.com>
2022-08-27 13:53:04 +1000

    add dwmblocks

---
+++ .suckless/dwmblocks/blocks.def.h
@@ -0,0 +1,14 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"",   "date '+%a %r %F'",		                                    	    1,		0},
+	{" ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	        10,		14},
+	{" ", "amixer sget Master | awk -F\"[][]\" '/%/ { print $2 }' | head -n1", 0,		10},
+	{" ", "echo -n \"$(cat /sys/class/power_supply/BAT1/capacity)%\"",	        120,		3},
+    {" ", "echo -n \"$(($(brightnessctl get)*100/$(brightnessctl max)))%\"",   0,      10},
+
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim[] = " | ";
+static unsigned int delimLen = 5;
