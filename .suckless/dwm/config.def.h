60802523b66db19413eba88e69ad5cb27f7cd3e8
MonkieeBoi <danny.pham26425@gmail.com>
2022-10-27 21:42:58 +1100

    fixed overlapping keybind

--- .suckless/dwm/config.def.h
+++ .suckless/dwm/config.def.h
@@ -112,7 +112,7 @@ static const Key keys[] = {
 	{ MODKEY|ShiftMask,      XK_j,      movestack,      {.i = +1 } },
 	{ MODKEY|ShiftMask,      XK_k,      movestack,      {.i = -1 } },
 	{ MODKEY,                XK_Return, zoom,           {0} },
-    { MODKEY,                XK_t,      togglesystray,  {0} },
+    { MODKEY,                XK_y,      togglesystray,  {0} },
 	{ MODKEY,                XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,      XK_c,      killclient,     {0} },
 	{ MODKEY,                XK_t,      setlayout,      {.v = &layouts[0]} },
