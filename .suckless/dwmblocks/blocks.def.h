97cbdc360626f845eed2cb4ed98e38881e9a51c4
MonkieeBoi <danny.pham26425@gmail.com>
2022-10-29 21:58:29 +1100

    change cursor colour and decrease battery refresh time

--- .suckless/dwmblocks/blocks.def.h
+++ .suckless/dwmblocks/blocks.def.h
@@ -6,7 +6,7 @@ static const Block blocks[] = {
     {" ",  "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	                                   20,    0},
     {" ",  "top -bn 1 | awk '/^%Cpu/ {print int($2 + $4 + $6)\"%\"}'",	                                   20,    0},
     {"",    "amixer -M sget Master | awk -F '[][]' '/%/ { print $4 $2 }' | sed 's/on/ /;s/off.*/ﱝ/;1q'",   0,    1},
-    {"  ", "echo -n \"$(cat /sys/class/power_supply/BAT1/capacity)%\"",                                  300,    0},
+    {"  ", "echo -n \"$(cat /sys/class/power_supply/BAT1/capacity)%\"",                                  150,    0},
     {" ",  "echo -n \"$(($(brightnessctl get)*100/$(brightnessctl max)))% \"",                             0,    2},
 
 };
