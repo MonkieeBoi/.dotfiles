3d962e1380c7b8a393d88831b1fb7815a1ba5c2d
MonkieeBoi <danny.pham26425@gmail.com>
2022-10-31 18:19:14 +1100

    ocd

--- .suckless/dwmblocks/blocks.def.h
+++ .suckless/dwmblocks/blocks.def.h
@@ -5,7 +5,7 @@ static const Block blocks[] = {
     {"",    "date '+%a %r %d/%m/%Y'",                                                                       1,    0},
     {"",    "cat /tmp/recordingicon",                                                                       0,    4},
     {"",    "wificonnection",                                                                              60,    0},
-    {" ",  "free -h | awk '/^Mem/ { print $3 }' | sed s/i//g",                                     30,    0},
+    {" ",  "free -h | awk '/^Mem/ { print $3 }' | sed s/i//g",                                            30,    0},
     {" ",  "top -bn 1 | awk '/^%Cpu/ {print int($2 + $4 + $6)\"%\"}'",                                    30,    0},
     {"",    "amixer -M sget Master | awk -F '[][]' '/%/ { print $4 $2 }' | sed 's/on/ /;s/off.*/ﱝ/;1q'",   0,    1},
     {"",    "battery",                                                                                     90,    0},
