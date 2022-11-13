2259884012c96fb1873dfec09ed9b1adae08f72e
MonkieeBoi <danny.pham26425@gmail.com>
2022-11-13 18:10:17 +1100

    add normie mode

---
+++ .suckless/dwmblocks/vanityblocks.def.h
@@ -0,0 +1,20 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+    // {"",   "cmusinfo",                                                                                      1,    3},
+    {"",    "cat /tmp/recordingicon",                                                                         0,    4},
+    {"^c#4c566a^^b#4c566a^^c#d8dee9^  ",  "date '+%a %R'",                                                                                1,    0},
+    {"^c#3b4252^^b#3b4252^^c#d8dee9^  ",  "free -h | awk '/^Mem/ { print $3 }' | sed s/i//g",                                            30,    0},
+    {"^c#4c566a^^b#4c566a^^c#d8dee9^  ",  "top -bn 1 | awk '/^%Cpu/ {print int($2 + $4 + $6)\"%\"}'",                                    30,    0},
+    {"^c#3b4252^^b#3b4252^^c#d8dee9^ ",    "amixer -M sget Master | awk -F '[][]' '/%/ { print $4 $2 }' | sed 's/on/ /;s/off.*/ﱝ/;1q'",   0,    1},
+    {"^c#4c566a^^b#4c566a^^c#d8dee9^ ",    "battery",                                                                                     90,    0},
+    {"^c#3b4252^^b#3b4252^^c#d8dee9^  ",  "echo \"$(xbacklight -get | cut -f1 -d '.')%\"",                                                0,    2},
+    {"^c#4c566a^^b#4c566a^^c#d8dee9^  ",  "date '+%d.%m.%y'",                                                                             1,    0},
+    {"^c#3b4252^^b#3b4252^^c#d8dee9^ ",    "wificonnection",                                                                              60,    0},
+
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim[] = " ";
+static unsigned int delimLen = 1;
+
