1f988ae0f6e4532e95a019588a249ba7440b8e6f
MonkieeBoi <danny.pham26425@gmail.com>
2023-04-16 14:14:24 +1000

    scuffed status2d merge with splitstatus for dwm

--- .suckless/dwmblocks/blocks.def.h
+++ .suckless/dwmblocks/blocks.def.h
@@ -1,16 +1,15 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-    // {"",   "cmusinfo",                                                                                      1,    3},
-    {" ",  "date '+%a %r|'",                                                                                 1,    0},
-    {"",    "cat /tmp/recordingicon",                                                                         0,    4},
-    {"  ",  "free -h | awk '/^Mem/ { print $3 }' | sed s/i//g",                                            30,    0},
-    {"  ",  "top -bn 1 | awk '/^%Cpu/ {print int($2 + $4 + $6)\"%\"}'",                                    30,    0},
-    {" ",    "amixer -M sget Master | awk -F '[][]' '/%/ { print $4 $2 }' | sed 's/on/ /;s/off.*/ﱝ/;1q'",   0,    1},
-    {" ",    "battery",                                                                                     90,    0},
-    // {"  ",  "echo \"$(xbacklight -get | cut -f1 -d '.')%\"",                                                0,    2},
-    {"  ",  "date '+%d.%m.%y'",                                                                             1,    0},
-    {" ",    "wificonnection",                                                                              60,    0},
+    {"^c#2e3440^^b#4c566a^^c#d8dee9^  ",  "date '+%a %r ^c#2e3440^|'",                                                                   1,    0},
+    {"",    "cat /tmp/recordingicon",                                                                                                       0,    4},
+    {"^c#4c566a^^b#4c566a^^c#d8dee9^  ",  "free -h | awk '/^Mem/ { print $3 }' | sed s/i//g",                                            30,    0},
+    {"^c#3b4252^^b#3b4252^^c#d8dee9^  ",  "top -bn 1 | awk '/^%Cpu/ {print int($2 + $4 + $6)\"%\"}'",                                    30,    0},
+    {"^c#4c566a^^b#4c566a^^c#d8dee9^ ",    "amixer -M sget Master | awk -F '[][]' '/%/ { print $4 $2 }' | sed 's/on/ /;s/off.*/ﱝ/;1q'",   0,    1},
+    {"^c#3b4252^^b#3b4252^^c#d8dee9^ ",    "battery",                                                                                     90,    0},
+    // {"  ",  "echo \"$(xbacklight -get | cut -f1 -d '.')%\"",                                                                              0,    2},
+    {"^c#4c566a^^b#4c566a^^c#d8dee9^  ",  "date '+%d.%m.%y'",                                                                             1,    0},
+    {"^c#3b4252^^b#3b4252^^c#d8dee9^ ",    "wificonnection",                                                                              60,    0},
 
 };
 
