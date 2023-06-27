bb7f94073ee8cf3ad81e82a973e6ee76d3d72509
MonkieeBoi <danny.pham26425@gmail.com>
2023-06-27 15:39:04 +1000

    add fuzzy matching to dmenu

--- .suckless/dmenu/config.def.h
+++ .suckless/dmenu/config.def.h
@@ -2,17 +2,20 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=9",
-	"NotoColorEmoji:pixelsize=13:antialias=true:autohint=true"
+    "JetBrains Mono Nerd Font:size=9",
+    "NotoColorEmoji:pixelsize=13:antialias=true:autohint=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#d8dee9", "#2e3440" },
-	[SchemeSel] = { "#eceff4", "#5381ac" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+    /*                        fg         bg       */
+    [SchemeNorm] =          { "#d8dee9", "#2e3440" },
+    [SchemeSel] =           { "#eceff4", "#5381ac" },
+    [SchemeSelHighlight] =  { "#a3be8c", "#5381ac" },
+    [SchemeNormHighlight] = { "#a3be8c", "#2e3440" },
+    [SchemeOut] =           { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
